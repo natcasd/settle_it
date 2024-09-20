28b4bf0f4f5ecf87a8ac7f585d99cb47872b2616
natcasd <nathan_depiero@brown.edu>
2024-09-19 23:58:30 -0400

    HelpModal

--- components/HelpModal.tsx
+++ components/HelpModal.tsx
@@ -5,7 +5,9 @@ import {
   ModalHeader,
   ModalBody,
   ModalCloseButton,
+  Box,
   Text,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import ledger_example from "../public/images/ledger_example.jpg";
 import Image from "next/image";
@@ -16,17 +18,21 @@ type HelpModalProps = {
 };
 
 const HelpModal: React.FC<HelpModalProps> = ({ isOpen, onClose }) => {
+  const modalSize = useBreakpointValue({ base: "xxs", md: "3xl" });
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="3xl">
       <ModalOverlay />
-      <ModalContent p={6}>
-        <ModalHeader fontWeight="bold" mb={4}>
-          How to use...
-        </ModalHeader>
-        <ModalCloseButton />
+      <ModalContent p={3}>
+        <Box bg="gray.100" p={4} position="relative" borderRadius="md">
+          <ModalCloseButton />
+          <ModalHeader fontWeight="bold" m={0} p={0} textAlign="center">
+            How to use...
+          </ModalHeader>
+        </Box>
         <ModalBody>
           <Text mb={4}>
-            1. For a{" "}
+            <b>1.</b> For a{" "}
             <a
               href="https://www.pokernow.club"
               className="text-blue-500 underline"
@@ -37,15 +43,17 @@ const HelpModal: React.FC<HelpModalProps> = ({ isOpen, onClose }) => {
             the bottom left of the table.
           </Text>
           <Text mb={4}>
-            2. On the "Session Ledger" page, copy the information like below.
+            <b>2.</b> On the "Session Ledger" page, copy the information like
+            below.
           </Text>
           <Image
             src={ledger_example}
             alt="example of what to copy in ledger"
+            className="rounded-md"
           ></Image>
           <Text mb={4} mt={4}>
-            3. Paste this data into the text box, and click calculate to get the
-            credits/debts between players.
+            <b>3.</b> Paste this data into the text box, and click calculate to
+            get the credits/debts between players.
           </Text>
         </ModalBody>
       </ModalContent>
